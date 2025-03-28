95ca328970235bcef043d3243b7a68c8804f164c
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-03-28 12:38:08 +0330

    integrating project with AI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-function generatePoem(event) {
-  event.preventDefault();
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "b318adc2a4e42ff5a70at1cf59b30fbo";
+  let prompt = `User instructions: Generate a french poem about ${instructionsInput.value}`;
+  let context =
+    "You are a romantic Poem expert and love to write short poem. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("poem-generator-form");
