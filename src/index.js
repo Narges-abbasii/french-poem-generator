d68b5c84c3a2277dbeb70660e0a22c3c05b7538e
Narges Abbasi <nargesabbasi2976@gmail.com>
2025-03-28 13:42:38 +0330

    updating style

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "b318adc2a4e42ff5a70at1cf59b30fbo";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in HTML fomat like <p></p> and separate each line with a <br /> and mention this poem belong to who in the end. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. You mission is to generate a 4 line poem in HTML fomat like <p></p> and separate each line with a <br />. Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a Persian poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
